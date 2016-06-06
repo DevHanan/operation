b7b5f42d36e89cc19d45e0aeef79efc0ad3b0b5f
amrfayad <amr.fci2007@gmail.com>
2016-06-07 00:05:09 +0200

    Start to handle fast spring notifiactions

--- app/Http/Controllers/NotificationsController.php
+++ app/Http/Controllers/NotificationsController.php
@@ -11,20 +11,82 @@ class NotificationsController extends Controller
     //
     public function subscriptionActivated(){
         
+        /*$privatekey = "82ee30b29867559d76c9f1d127725e8e";
+        if (md5($_REQUEST["security_data"] . $privatekey) != $_REQUEST["security_hash"]) {
+            return "Invalid data"; 
+        }*/
+        $file = "active.txt";
+        $data = fopen($file, "a") or die("Can't open file.");
+        foreach ($_REQUEST as $key => $value) {
+                     fwrite($data, $key);
+                     fwrite($data,"=");
+                     
+                     fwrite($data, $value);
+                     fwrite($data,"\n");
+                     
+         }
+        fclose($data);
+        
         return "Subscription Activated"; 
     }
      public function subscriptionDeactivated(){
         
+        /*$privatekey = "82ee30b29867559d76c9f1d127725e8e";
+        if (md5($_REQUEST["security_data"] . $privatekey) != $_REQUEST["security_hash"]) {
+            return "Invalid data"; 
+        }*/
+        $file = "deactive.txt";
+        $data = fopen($file, "a") or die("Can't open file.");
+        foreach ($_REQUEST as $key => $value) {
+                     fwrite($data, $key);
+                     fwrite($data,"=");
+                     
+                     fwrite($data, $value);
+                     fwrite($data,"\n");
+                     
+         }
+        fclose($data);
          return "Subscription Deactivated"; 
    
     }
      public function subscriptionChanged(){
         
+       /*$privatekey = "82ee30b29867559d76c9f1d127725e8e";
+        if (md5($_REQUEST["security_data"] . $privatekey) != $_REQUEST["security_hash"]) {
+            return "Invalid data"; 
+        }*/
+        $file = "changed.txt";
+        $data = fopen($file, "a") or die("Can't open file.");
+        foreach ($_REQUEST as $key => $value) {
+                     fwrite($data, $key);
+                     fwrite($data,"=");
+                     
+                     fwrite($data, $value);
+                     fwrite($data,"\n");
+                     
+         }
+        fclose($data);
          return "Subscription Changed"; 
    
     }
      public function paymentFailed(){
-         return "Payement Failed"; 
+        
+        /*$privatekey = "82ee30b29867559d76c9f1d127725e8e";
+        if (md5($_REQUEST["security_data"] . $privatekey) != $_REQUEST["security_hash"]) {
+            return "Invalid data"; 
+        }*/
+        $file = "failed.txt";
+        $data = fopen($file, "a") or die("Can't open file.");
+        foreach ($_REQUEST as $key => $value) {
+                     fwrite($data, $key);
+                     fwrite($data,"=");
+                     
+                     fwrite($data, $value);
+                     fwrite($data,"\n");
+                     
+                  }
+        fclose($data);
+        return "Payement Failed"; 
    }
     
 }
